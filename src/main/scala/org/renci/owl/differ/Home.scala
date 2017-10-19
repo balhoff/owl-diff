60f51b3e11856eef52ae987a0979d5d2663a419b
Jim Balhoff <jim@balhoff.org>
2017-10-19 11:26:26 -0400

    Fixed page title.

--- src/main/scala/org/renci/owl/differ/Home.scala
+++ src/main/scala/org/renci/owl/differ/Home.scala
@@ -12,7 +12,7 @@ object Home {
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	  <title>Text and password inputs</title>
+	  <title>OWL diff</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
